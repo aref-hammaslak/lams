a3831c0cd965c7a2394bd5620282ce96adcf055b
aref-hammaslak <arefhammaslak@gmail.com>
2024-08-02 10:18:54 +0330

    Configure fonts for MUI

---
+++ client/src/theme.mui.js
@@ -0,0 +1,15 @@
+import {  createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'poppins',
+      'Roboto',
+      'sans-serif'
+      
+    ].join(','),
+  },
+});
+
+export default theme;
