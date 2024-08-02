7a8f8ddee8a98baf72e4e59c0e466daef2be139b
aref-hammaslak <arefhammaslak@gmail.com>
2024-08-02 10:17:11 +0330

    Add fonts; Configure Tailwindcss

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthProvider";
 import "./index.css";
+import "./tailwind.output.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import theme from "./theme.mui";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
@@ -14,9 +17,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthProvider>
          <BrowserRouter>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-               <Routes>
+               <ThemeProvider theme={theme}>
+                                 <Routes>
                   <Route path="/*" element={<App />}/>
                </Routes>
+               </ThemeProvider>
+
             </LocalizationProvider>
          </BrowserRouter>
       </AuthProvider>
