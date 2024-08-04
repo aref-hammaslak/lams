5969f27a10401a025b4f56fc00a2d4c4df0950a3
aref-hammaslak <arefhammaslak@gmail.com>
2024-08-04 19:06:14 +0330

    Add feature that admin and superviser be able to modify all equipment logs

--- server/models/EquipmentLog.js
+++ server/models/EquipmentLog.js
@@ -4,6 +4,7 @@ import LogTemplate from "./LogTemplate.js";
 
 import { getScheduleMapsForDate } from "../services/scheduleMap.js";
 import { getLogTemplateItems } from "../services/logTemplate.js";
+import User from "./User.js";
 
 const logSchema = new Schema({
     date: {
@@ -21,6 +22,11 @@ const logSchema = new Schema({
         required: true,
         immutable: true
     },
+    sch_id:{
+        type: Schema.Types.ObjectId,
+        ref: 'Schedule',
+        required: true
+    },
     items:
     {
         type: Schema.Types.Mixed,
@@ -30,6 +36,9 @@ const logSchema = new Schema({
 
 logSchema.index({ date: 1, temp_id: 1 }, { unique: true });
 
+// Validates the date field of the equipment log document.
+// This function checks if a schedule map exists for the specified date, user, and log template.
+// If the user is a supervisor or admin, the schedule map validation is skipped.
 logSchema.path('date').validate(async function (date) {
     let origDoc;
     if (!('_id' in this)) {
@@ -37,6 +46,13 @@ logSchema.path('date').validate(async function (date) {
     }
 
     const user_id = this.get('user_id') ?? origDoc.get('user_id');
+    // check if user is supervisor of admin skip the validation schedule map
+    const user = await User.findById(user_id);
+    if (!user) {
+        throw new Error('Failed to find user');
+    }
+    if (user.roles.includes(2005) || user.roles.includes(1923)) return true;
+
     const temp_id = this.get('temp_id') ?? origDoc.get('temp_id');
 
     const maps = await getScheduleMapsForDate(date, user_id, 'equipment', temp_id);
@@ -55,6 +71,14 @@ logSchema.path('user_id').validate(async function (user_id) {
 
     const date = this.get('date') ?? origDoc.get('date');
     const temp_id = this.get('temp_id') ?? origDoc.get('temp_id');
+
+
+    // check if user is supervisor of admin skip the validation schedule map
+    const user = await User.findById(user_id);
+    if (!user) {
+        throw new Error('Failed to find user');
+    }
+    if (user.roles.includes(2005) || user.roles.includes(1923)) return true;
 
     const maps = await getScheduleMapsForDate(date, user_id, 'equipment', temp_id);
     if (maps.length == 0) {
