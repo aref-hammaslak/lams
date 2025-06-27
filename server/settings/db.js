a8831371511132482a33ccc0474e6e21223d72e7
aref-hammaslak <arefhammaslak@gmail.com>
2025-06-27 18:46:53 +0330

    Enhance database connection settings

--- server/settings/db.js
+++ server/settings/db.js
@@ -4,7 +4,11 @@ import { logger } from "./logger.js";
 
 const dbUri = config.get('dbUri');
 
-await mongoose.connect(dbUri);
+console.log('Database URI: ',dbUri);
+await mongoose.connect(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 db.on('error', err => logger.error("Database Connection Failure", { err }));
@@ -12,4 +16,4 @@ db.on('error', err => logger.error("Database Connection Failure", { err }));
 db.on('open', () => logger.info("Database Connection Established"));
 
 export default db; 
-export {db}; 
+export {db};
